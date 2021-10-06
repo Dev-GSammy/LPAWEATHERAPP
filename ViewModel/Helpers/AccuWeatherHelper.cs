ce25b86cc483e21971186257098fbeec24e11db0
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2021-10-06 09:30:11 +0100

    received  response from url and json format respecively

--- ViewModel/Helpers/AccuWeatherHelper.cs
+++ ViewModel/Helpers/AccuWeatherHelper.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using LPAWEATHERAPP.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,8 +13,22 @@ namespace LPAWEATHERAPP.ViewModel.Helpers
         //the class has to be made public because it obviously will be used from different namespaces.
         public const string BASE_URL = "http://dataservice.accuweather.com/";
         public const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
-        public const string CURRENT_CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey=cgR1NRAcWK52h1rNVC2ARxGfgt7ADZ1X";
+        public const string CURRENT_CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
         public const string API_KEY = "cgR1NRAcWK52h1rNVC2ARxGfgt7ADZ1X";
 
+
+        public static async List<City> GetCities(string query)
+        {
+            List<City> cities = new List<City>();
+
+            string url = BASE_URL + string.Format(AUTOCOMPLETE_ENDPOINT, API_KEY, query);
+            using(HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+            }
+
+            return cities;
+        }
     }
 }
