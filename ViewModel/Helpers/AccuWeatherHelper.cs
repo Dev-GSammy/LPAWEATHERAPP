1f91e2d58e9e750061af23c32e17cf424986333d
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2021-10-06 09:11:56 +0100

    Successfully retrieved endpoint from the current condition API

--- ViewModel/Helpers/AccuWeatherHelper.cs
+++ ViewModel/Helpers/AccuWeatherHelper.cs
@@ -11,7 +11,7 @@ namespace LPAWEATHERAPP.ViewModel.Helpers
         //the class has to be made public because it obviously will be used from different namespaces.
         public const string BASE_URL = "http://dataservice.accuweather.com/";
         public const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
-        public const string CURRENT_CONDITIONS_ENDPOINT = "";
+        public const string CURRENT_CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey=cgR1NRAcWK52h1rNVC2ARxGfgt7ADZ1X";
         public const string API_KEY = "cgR1NRAcWK52h1rNVC2ARxGfgt7ADZ1X";
 
     }
