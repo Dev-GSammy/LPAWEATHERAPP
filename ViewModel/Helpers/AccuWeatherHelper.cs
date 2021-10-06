a1c95710405d5e66ae24eb1dac34ac6ef7c16884
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2021-10-06 07:29:25 +0100

    Created a new class for the helper folder

---
+++ ViewModel/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPAWEATHERAPP.ViewModel.Helpers
+{
+    public class AccuWeatherHelper
+    {
+        //the class has to be made public because it obviously will be used from different namespaces.
+    }
+}
